4801da4c74d72550d49774f7facb804af951f0a9
dakoto <dakoto747@gmail.com>
2020-07-30 16:00:43 +0100

    update ...

--- vmlp.py
+++ vmlp.py
@@ -44,26 +44,15 @@ class vmlp(object):
             self.layer_neuron_count = [self.input_layer_neuron_count] + [self.input_layer_neuron_count] +[1]
 
         for i in range(0, self.layer_count):
-            # numpy.random.random(rows, cols)
             # initializing weights of vectors/neurons
             layer_neurons = numpy.matrix( numpy.random.random((self.layer_neuron_count[i+1], self.layer_neuron_count[i]+1)))
             self.neurons.append(layer_neurons)
             self.weight_updates.append(layer_neurons)
-        # shape is a property of both numpy ndarray's and matrices.
-        # A.shape
-        # will return a tuple (m, n), where m is the number of rows, and n is the number of columns
 
     def feedForward(self, input_):
         self.layer_outputs = []
         self.layer_outputs.append(input_)
         for layer in range(0, self.layer_count):
-            # if z == self.layer_count-1:
-            #     self.layer_outputs[i+1] = self.layer_outputs[z] * self.neurons[z].T# i+1 because we want the input layer untouched
-            # else:
-
-            # v = np.ones((10, 1))
-            # c = np.c_[m, v]
-
             inp = numpy.c_[self.layer_outputs[layer], 1]
             self.layer_outputs.append(self.numpySigmoid(inp * self.neurons[layer].T))
         
@@ -76,16 +65,7 @@ class vmlp(object):
         self.layer_deltas.append(output_delta)
 
         output_delta_w = self.learning_rate * output_delta * numpy.c_[self.layer_outputs[self.layer_count-1], 1]
-        # output_delta_w = self.learning_rate * output_delta * self.layer_outputs[self.layer_count-1]
-        # print (output_delta_w)
-        # print(self.neurons)
         for i in range(self.layer_count, 1, -1):
-            # print(self.layer_neuron_count[i])
-            # print(self.layer_outputs[i-1])
-            # print '--'
-            # print(self.neurons[i-1])
-            # print((self.neurons[i-1][:,0:self.layer_neuron_count[i-1]]).T)
-            # print((self.layer_deltas[0][0,0] * self.neurons[i-1][:,0:self.layer_neuron_count[i-1]]).T)
 
             self.layer_deltas.insert(
                 0, # input it in position 0 because we're iterating backwards in terms of layers 
@@ -97,18 +77,8 @@ class vmlp(object):
                 )
             ) # bias
 
-            # print(self.layer_deltas[0])
-
             delta_w = self.learning_rate * self.layer_deltas[0] * numpy.c_[self.layer_outputs[i-2], 1]
-            # print(delta_w)
-            # print '**'
-            # print(self.neurons[i-1])
-            # self.weight_updates[i-1] = delta_w
             self.neurons[i-2] =  delta_w + self.neurons[i-2]
-
-        # for i in range(self.layer_count, 1, -1):
-        #     self.neurons[i-1] = weight_updates[i-1] + self.neurons[i-1]
-        # print(self.neurons[self.layer_count-1])
 
         self.neurons[self.layer_count-1] = self.neurons[self.layer_count-1] + output_delta_w
 
@@ -118,9 +88,6 @@ class vmlp(object):
 
         for x in range(0, self.iterations):
             for y in range(0, self.data.shape[0]):
-                # v = np.ones((10, 1))
-                # c = np.c_[m, v]
-
                 input_ = self.data[y, :]
                 self.feedForward(input_)
                 self.backpropInput(self.labels[y])
@@ -142,10 +109,6 @@ class vmlp(object):
         return 1.0 / ( 1.0 + math.exp(-x) )
 
     def predictedLabels(self):
-        # for y in range(0, data.shape[0]):
-            # input_ = self.data[y, :]
-            # self.feedForward(input_)
-            # print(self.layer_outputs[self.layer_count][0,0])
         self.patregTest(self.data, self.labels)
         print(self.raw_labels)
         
